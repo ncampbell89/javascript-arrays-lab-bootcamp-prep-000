9296033aff2ad27e0010054ddf28fe17f5979f7e
Nicole Campbell <nscampbell303@gmail.com>
2017-10-01 15:28:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.append("Ralph")
-  return kittens
+  return appendKitten(name)
 }
